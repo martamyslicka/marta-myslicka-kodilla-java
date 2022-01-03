e2ed2ec0f49e1c6df3d5f333843c37b92f1abf05
martamyslicka <martamyslicka@gmail.com>
2022-01-03 17:23:45 +0100

    8.4 Flight Searcher

---
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FlightSearcher.java
@@ -0,0 +1,33 @@
+package com.kodilla.exception.test;
+
+import java.lang.*;
+import java.util.*;
+
+public class FlightSearcher {
+
+    public void findFlight(Flight flight) throws RouteNotFoundException {
+        System.out.println("Searching for flight from "
+                + flight.getDepartureAirport() + " to " + flight.getArrivalAirport());
+
+        Map<String, Boolean> directions = new HashMap<>();
+        directions.put("Warsaw", true);
+        directions.put("Praha", false);
+        directions.put("Krakow", true);
+        directions.put("Gdansk", false);
+        directions.put("Lodz", true);
+
+        String result = flight.getArrivalAirport();
+
+        if (directions.containsKey(result)) {
+            if (!directions.get(result)) {
+                System.out.println("The airport " + result + "is not available for the moment");
+            } else {
+                System.out.println("Founded flight to: " + result);
+            }
+        } else {
+            throw new RouteNotFoundException("There are no flights to: " + result);
+        }
+
+    }
+}
+
---
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FlightSearcherRunner.java
@@ -0,0 +1,22 @@
+package com.kodilla.exception.test;
+
+public class FlightSearcherRunner {
+    public static void main(String args[]) {
+
+        Flight flight = new Flight("Warsaw", "Krakow");
+        Flight flight2 = new Flight("Warsaw", "Maroko");
+
+        FlightSearcher flightFinder = new FlightSearcher();
+        try {
+            flightFinder.findFlight(flight);
+        } catch (RouteNotFoundException e) {
+            System.out.println(e);
+        }
+
+        try {
+            flightFinder.findFlight(flight2);
+        } catch (RouteNotFoundException e) {
+            System.out.println(e);
+        }
+    }
+}
