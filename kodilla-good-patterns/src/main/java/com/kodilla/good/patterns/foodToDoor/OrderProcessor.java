777364098aeac17c878b50ceceb27f13d103af26
martamyslicka <martamyslicka@gmail.com>
2022-01-08 00:05:27 +0100

    9.4: Food2Door - done.

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/foodToDoor/OrderProcessor.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/foodToDoor/OrderProcessor.java
@@ -15,18 +15,18 @@ public class OrderProcessor {
     }
 
     public void process(final List<OrderRequest> orderedList) {
-
         boolean isOrdered = orderService.order(orderedList);
-
-        if (isOrdered) {
+        if (isOrdered && orderedList.size() > 0) {
             for (OrderRequest order : orderedList) {
                 orderRepository.createOrder(orderedList);
-                }
-                System.out.println("Order proceeding is finished ");
-
             }
+            System.out.println("Order proceeding is finished ");
 
+        } else {
+            System.out.println("Meals that you have ordered are not provided by our suppliers.");
         }
 
 
     }
+}
+
--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/foodToDoor/OrderRequestRetriever.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/foodToDoor/OrderRequestRetriever.java
@@ -13,15 +13,15 @@ public class OrderRequestRetriever {
     }
 
     public void createOrderedList() {
-        OrderRequest one = new OrderRequest("Healthy Shop", "meal1", 2) ;
-        OrderRequest two = new OrderRequest("Gluten Free Shop", "meal11", 25);
-        OrderRequest three = new OrderRequest("Szczedrzykowice Shop", "something", 4);
-        OrderRequest four = new OrderRequest("Healthy Shop", "meal44", 2) ;
+        //OrderRequest one = new OrderRequest("Healthy Shop", "meal1", 2) ;
+        //OrderRequest two = new OrderRequest("Gluten Free Shop", "meal11", 25);
+        //OrderRequest three = new OrderRequest("Szczedrzykowice Shop", "something", 4);
+        //OrderRequest four = new OrderRequest("Healthy Shop", "meal44", 2) ;
 
-        orderedList.add(one);
-        orderedList.add(two);
-        orderedList.add(three);
-        orderedList.add(four);
+        //orderedList.add(one);
+        //orderedList.add(two);
+        //orderedList.add(three);
+        //orderedList.add(four);
     }
 
     public List<OrderRequest> getOrderedProductList() {
